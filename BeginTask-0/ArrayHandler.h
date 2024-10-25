18a01e24c6e67c1306edf0e9864433fde7928f0f
crn4t <74592714+crn4tt@users.noreply.github.com>
2024-10-25 15:19:35 +0300

    Update ArrayHandler.h

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -4,21 +4,6 @@ template<typename T>
 struct ArrayHandler;
 
 template<>
-struct ArrayHandler<uint8_t> {
-    static constexpr uint8_t GetMax() noexcept {
-        return 10;
-    }
-
-    static constexpr uint8_t GetMin() noexcept {
-        return 1;
-    }
-
-    static void AppendElem(uint8_t /*elem*/) noexcept {
-     
-    }
-};
-
-template<>
 struct ArrayHandler<uint32_t> {
     static constexpr uint32_t GetMax() noexcept {
         return 1000000010;
