a4f65523de7f781abe3bf13e6587f0ea4ad95507
crn4t <74592714+crn4tt@users.noreply.github.com>
2024-10-25 14:56:04 +0300

    sdla;ksd

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -1,44 +1,26 @@
 #include <cstdint>
-#include <limits>
-#include <cstdlib>
 
 template<typename T>
-class ArrayHandler {
-private:
-    T _array[1000000]; 
-    size_t _count;
-    T _min;
-    T _max;
+struct ArrayHandler;
 
-public:
-    ArrayHandler()
-        : _count(0), _min(std::numeric_limits<T>::max()), _max(std::numeric_limits<T>::min()) {}
-
-    inline void AppendElem(T elem) {
-        _array[_count++] = elem;
-
-        _min = (elem < _min) ? elem : _min;
-        _max = (elem > _max) ? elem : _max;
+template<>
+struct ArrayHandler<uint8_t> {
+    static constexpr uint8_t GetMax() noexcept {
+        return 10;
     }
 
-    inline T GetMax() const {
-        return _max;
+    static constexpr uint8_t GetMin() noexcept {
+        return 1;
+    }
+};
+
+template<>
+struct ArrayHandler<uint32_t> {
+    static constexpr uint32_t GetMax() noexcept {
+        return 1000000010;
     }
 
-    inline T GetMin() const {
-        return _min;
-    }
-
-    bool IsContains(const T* elem) const {
-        const T* end = _array + _count;
-        const T* ptr = _array;
-
-        while (ptr < end) {
-            if (*ptr == *elem) {
-                return true;
-            }
-            ++ptr;
-        }
-        return false;
+    static constexpr uint32_t GetMin() noexcept {
+        return 0;
     }
 };
