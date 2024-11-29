68a7deb7d1de3adaad4cfd5fcd02b6bbde207c38
crn4t <74592714+crn4tt@users.noreply.github.com>
2024-11-29 14:50:34 +0300

    Update BitField.h

--- BitField-2/BitField.h
+++ BitField-2/BitField.h
@@ -1,33 +1,48 @@
 #pragma once
 #include <iostream>
+#include <vector>
 #include <stdint.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+
 
 class BitField {
 private:
+    size_t _sizeMem;
     size_t _sizeBit;
     uint16_t* _mem;
-    size_t _memSize;
-
-    size_t GetMemIndex(size_t n) const;
-    uint16_t GetMask(size_t n) const;
-public:
-    BitField(size_t len);
-    BitField(const BitField& tmp);
-    BitField& operator=(const BitField& tmp);
     
-    size_t GetLength() const;
+    size_t GetMemIndex(size_t n)const;
+    uint16_t GetMask(size_t n) const;
+
+public:
+    BitField(size_t sizeBit);
+    ~BitField();
+    BitField(const BitField& tmp);
+
     void SetBit(size_t n);
     void ClrBit(size_t n);
     uint8_t GetBit(size_t n) const;
-
-
-    BitField operator|(const BitField& tmp);
-    BitField operator&(const BitField& tmp);
-    BitField operator^(const BitField& tmp);
+    size_t GetLength() const;
+    
     bool operator==(const BitField& tmp) const;
+    BitField& operator=(const BitField& tmp);
+    BitField operator^(const BitField& tmp);
+    BitField operator&(const BitField& tmp);
+    BitField operator|(const BitField& tmp);
     BitField operator~();
+    
+    BitField& Universe();
 
-    ~BitField(){
+    friend istream& operator>>(istream& in, BitField& x);
+    friend ostream& operator<<(ostream& os, const BitField& x);
 
-    }
 };
