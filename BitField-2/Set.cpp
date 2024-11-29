2caa91e903feacaba5066a172e526bbd06037f32
crn4t <74592714+crn4tt@users.noreply.github.com>
2024-11-29 14:53:12 +0300

    Update Set.cpp

--- BitField-2/Set.cpp
+++ BitField-2/Set.cpp
@@ -1,48 +1,111 @@
-#pragma once
-#include <iostream>
-#include <vector>
-#include <stdint.h>
-#include <unordered_map>
-#include <unordered_set>
-#include <algorithm>
-#include <cstdlib>
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string>
+#include "Set.h"
 
-using namespace std;
+Set::operator BitField() {
+    return BitField(this->_bitfield);
+}
+
+Set::Set(const BitField& tmp) : _maxPower(tmp.GetLength()), _bitfield(tmp) {}
+
+Set::Set(const Set& tmp):_maxPower(tmp.GetMaxPow()), _bitfield(tmp._bitfield) {}
+
+Set::Set(const size_t maxPower) :_bitfield(maxPower) { _maxPower = maxPower; }
+
+void Set::InsElem(uint64_t elem) {
+    if (elem < _maxPower)
+        _bitfield.SetBit(elem);
+    else {
+        throw "Elem out of range";
+    }
+}
+
+void Set::DelElem(uint64_t elem) {
+    _bitfield.ClrBit(elem);
+}
+
+size_t Set::GetMaxPow() const {
+    return _maxPower;
+}
+
+bool Set::IsMember(uint64_t elem) {
+    if (elem < _maxPower && elem >= 0){
+        if (_bitfield.GetBit(elem) == 1) return true;
+        }
+        return false;
+}
 
 
-class BitField {
-private:
-    size_t _sizeMem;
-    size_t _sizeBit;
-    uint16_t* _mem;
+bool Set::operator==(const Set& tmp) const {
+    if (_bitfield == tmp._bitfield)
+        return true;
+    return false;
+}
+
+bool Set::operator!=(const Set& tmp) const{
+    if (_bitfield == tmp._bitfield)
+        return false;
+    return true;
+}
+
+Set& Set::operator=(const Set& tmp) {
+    _maxPower = tmp._maxPower;
+    _bitfield = tmp._bitfield;
+    return *this;
+}
+
+Set Set::operator+(const Set& tmp) {
+    Set result(max(tmp._maxPower, _maxPower));
+    result._bitfield = _bitfield | tmp._bitfield;
+    return result;
+}
+
+vector<uint64_t> Set::GetPrimary() const {
+    vector <uint64_t> res;
+    unordered_set<uint64_t> used;
+    res.push_back(1);
+    for (uint64_t i = 2; i < _maxPower + 1; i++){
     
-    size_t GetMemIndex(size_t n)const;
-    uint16_t GetMask(size_t n) const;
-
-public:
-    BitField(size_t sizeBit);
-    ~BitField();
-    BitField(const BitField& tmp);
-
-    void SetBit(size_t n);
-    void ClrBit(size_t n);
-    uint8_t GetBit(size_t n) const;
-    size_t GetLength() const;
+        if (used.find(i) != used.end()) continue;
     
-    bool operator==(const BitField& tmp) const;
-    BitField& operator=(const BitField& tmp);
-    BitField operator^(const BitField& tmp);
-    BitField operator&(const BitField& tmp);
-    BitField operator|(const BitField& tmp);
-    BitField operator~();
-    
-    BitField& Universe();
+        else{
+            res.push_back(i);
+            for (uint64_t j = i + 1; j < _maxPower + 1; j++)
+                if (j % i == 0) used.insert(j);
+            
+       }
+    }
+    return res;
+}
 
-    friend istream& operator>>(istream& in, BitField& x);
-    friend ostream& operator<<(ostream& os, const BitField& x);
 
+void Set::operator+(uint64_t elem) {
+    InsElem(elem);
+}
+
+void Set::operator-(uint64_t elem) {
+    DelElem(elem);
+}
+
+Set Set::operator*(const Set& tmp) {
+    Set result(max(tmp._maxPower, _maxPower));
+    result._bitfield = _bitfield & tmp._bitfield;
+    return result;
+}
+
+Set Set::operator~() {
+    Set result(_maxPower);
+    result._bitfield = ~_bitfield;
+    return result;
+}
+
+
+ostream& operator<<(ostream& os, const Set& tmp) {
+    os << tmp._bitfield;
+    return os << endl;
 };
+
+istream& operator>>(istream& is, Set& tmp) {
+    is >> tmp._maxPower;
+    is >> tmp._bitfield;
+    return is;
+}
+
